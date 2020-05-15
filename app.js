1c595c2a8d87d6b32c72fbb5dbcf6bf3fb63716e
Santi Balaguer <santiago.balaguer@gmail.com>
2020-05-15 15:24:53 +0100

    changed directory for images

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const { join } = require('path');
-const express = require('express');
-const createError = require('http-errors');
-const logger = require('morgan');
-const serveFavicon = require('serve-favicon');
-const indexRouter = require('./routes/index');
+const { join } = require("path");
+const express = require("express");
+const createError = require("http-errors");
+const logger = require("morgan");
+const serveFavicon = require("serve-favicon");
+const indexRouter = require("./routes/index");
 
-console.log('Hey class!')
+console.log("Hey class!");
 
 const app = express();
 
-app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
-app.use(express.static(join(__dirname, 'public')));
-app.use(logger('dev'));
+app.use(serveFavicon(join(__dirname, "public/imagesGoHere", "favicon.ico")));
+app.use(express.static(join(__dirname, "public")));
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 // Catch all error handler
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
-  res.sendFile(join(__dirname, './views', 'error.html'));
+  res.sendFile(join(__dirname, "./views", "error.html"));
 });
 
 module.exports = app;
